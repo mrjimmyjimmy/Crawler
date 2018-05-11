e2146ac8b7c65b830b92189fbb7ec96efeacea6d
Jimmy <mrxujimmy@gmail.com>
2018-05-11 16:38:19 +1000

    clean up

--- realestate-Mel/spider.py
+++ realestate-Mel/spider.py
@@ -4,11 +4,10 @@ import re
 import json
 from multiprocessing import Pool
 
-# 用来爬取realestate信息
 
+# 用来爬取realestate信息
 def get_one_page(url):
     try:
-
         response = requests.get(url)
         if response.status_code == 200:
             return response.text
@@ -18,27 +17,21 @@ def get_one_page(url):
 
 # 从返回的页面数据中用正则表达式提取所需信息
 def parse_one_page(html):
-    # re.S 表示可以匹配任意的字符
 
+    # re.S 表示可以匹配任意的字符
     pattern = re.compile('data-featured-status.*?<a href="(.*?)" >' +
-                         # '.*?openTime">(.*?)<' +
                          '.*?agent-photo" src="(.*?)"' +
                          '.*?title="(.*?)"' +
                          '.*?priceText">(.*?)<' +
                          '.*?listingName.>(.*?)<' +
                          '.*?Bedrooms</span></dt> <dd>(\d+)<' +
-                         '.*?Bathrooms</span></dt> <dd>(\d+)<'
-                         # '.*?Car Spaces</span></dt> <dd>(\d+)<'
-                         , re.S)
+                         '.*?Bathrooms</span></dt> <dd>(\d+)<', re.S)
     items = re.findall(pattern, html)
-    # print(items)
-
 
     # 格式化，变成字典
     for item in items:
         yield {
             'urlDetail': 'https://www.realestate.com.au' + item[0],
-            # 'openTime': item[1],
             'agentPic': item[1],
             'agent': item[2],
             'price': item[3],
@@ -54,7 +47,6 @@ def parse_one_page(html):
 # encoding = 'utf-8'
 # ensure_ascii = False
 # 确保写入的是中文而不是ascii码
-
 def write_to_file(content):
     with open('result.txt', 'a', encoding = 'utf-8') as f:
         f.write(json.dumps(content, ensure_ascii = False) + '\n')
@@ -64,14 +56,11 @@ def write_to_file(content):
 def main(list):
     url = 'https://www.realestate.com.au/rent/in-melbourne,+vic/list-' + str(list)
     html = get_one_page(url)
-    # parse_one_page(html)
-    print(html)
     for item in parse_one_page(html):
         print(item)
         write_to_file(item)
 
 # 单进程，速度慢
-
 # if __name__ == '__main__':
 #     for i in range(100):
 #         main(i)
@@ -79,4 +68,4 @@ def main(list):
 # 多进程，提升速度
 if __name__ == '__main__':
     pool = Pool()
-    pool.map(main, [i for i in range(1)])
+    pool.map(main, [i for i in range(100)])
